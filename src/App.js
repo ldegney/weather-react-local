2e0107ab38ab7edf2041137f41ab407db9d03790
ldegney <degsno1@gmail.com>
2025-02-20 20:13:32 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import Weather from "./Weather"
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Hello From React</h1>
-      <Weather/>
-      </header>
+      <div className="container">
+        <Weather defaultCity="San Francisco" />
+      </div>
     </div>
   );
 }
-
-export default App;
